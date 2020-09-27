c12ab8e36db8215143a18037082c4d7d8158ae55
Sabrina Blockwel <sblockwel@gmail.com>
2020-09-26 22:34:22 -0300

    Adicionado suporte ao EF Core e banco de dados SQLite

---
+++ Data/AppContext.cs
@@ -0,0 +1,14 @@
+﻿using AppBasquete.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppBasquete.Data
+{
+    class AppContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=c:\mydb.db;Version=3;");
+        }
+        public virtual DbSet<Jogo> Jogos { get; set; }
+    }
+}
---
+++ Models/Jogo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBasquete.Models
+{
+    class Jogo
+    {
+        public int Id { get; set; }
+        public int Placar { get; set; }
+        public int MinimoTemporada { get; set; }
+        public int MaximoTemporada { get; set; }
+        public int QuebraRecordeMinimo { get; set; }
+        public int QuebraRecordeMaximo { get; set; }
+
+    }
+}
